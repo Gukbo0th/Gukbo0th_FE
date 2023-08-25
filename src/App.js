86fc80a5fa9ca54597e0077f07eee1712f8a7da3
MinJaeSon <mjsonsj@gmail.com>
2023-08-26 00:13:30 +0900

    [feat] 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import Funding from "./pages/Funding";
+import MyPage from "./pages/MyPage";
+import Login from "./pages/Login";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/funding" element={<Funding />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
